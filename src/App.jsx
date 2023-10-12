f4663302d8878452f6421f7f38051991fa2d881b
Chandanmajhi7 <chandanmajhi9693@gmail.com>
2023-10-12 22:53:36 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chandan Majhi",
+  title: "Web Designer & Application developer",
+  email: "CHANDAN.MAJHI@studentambassadors.com",
+  gitHub: "Chandanmajhi7",
+  instagram: "chandanmajhi.ai",
+  linkedIn: "chandan majhi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "chandanmajhi7",
+  youTube: "chandan majhi",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/computer.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "computer background";
 
 /**
  * Sort description that expands on your title on the Home component.
@@ -31,11 +31,11 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web development",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "AI/ML",
+  "mentoship",
+  "Mobile application development",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am Chandan Majhi, and I am passionate about using technology to make a positive impact in the world. From a young age, I have been fascinated by the power of technology to transform people's lives, and this has driven me to pursue a degree in B.Tech in Computer Science and Engineering..";
 
 const About = () => {
   return (
